20bc1b43aac242111549fbb49d94f82502e20d83
stefan hagiu <stefanhagiu@gmail.com>
2016-11-02 18:49:34 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,96 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+var messages = [];
+var sockets = [];
+
+io.on('connection', function (socket) {
+    messages.forEach(function (data) {
+      socket.emit('message', data);
+    });
+
+    sockets.push(socket);
+
+    socket.on('disconnect', function () {
+      sockets.splice(sockets.indexOf(socket), 1);
+      updateRoster();
+    });
+
+    socket.on('message', function (msg) {
+      
+      var jsonObj;
+      var message;
+      try {
+        jsonObj = (eval('('+msg+')')); //String(msg || '');
+        message = jsonObj.text;
+      }catch(e) {
+        console.log('we has error',e);
+        text = '';
+      }
+
+      if (!text)
+        return;
+
+      socket.get('name', function (err, name) {
+        
+        // eval over a var that wasn't sanitized
+        var data = {
+              name: name,
+              text: message
+            };
+
+        broadcast('message', data);
+        messages.push(data);
+      });
+    });
+
+    socket.on('identify', function (jsonObj) {
+      var jsonData = JSON.parse(jsonObj);
+      socket.set('name', String(jsonData.name || 'Anonymous'), function (err) {
+        updateRoster();
+      });
+    });
+  });
+
+function updateRoster() {
+  async.map(
+    sockets,
+    function (socket, callback) {
+      socket.get('name', callback);
+    },
+    function (err, names) {
+      broadcast('roster', names);
+    }
+  );
+}
+
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    socket.emit(event, data);
+  });
+}
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
